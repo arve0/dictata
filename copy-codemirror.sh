9639986d2e68d8e162558d67c6bd50fb78c92098
Arve Seljebu <arve.seljebu@gmail.com>
2018-09-13 19:47:00 +0200

    default styles with milligram

--- copy-codemirror.sh
+++
@@ -1,3 +0,0 @@
-cp node_modules/codemirror/lib/codemirror.js build
-cp node_modules/codemirror/lib/codemirror.css build
-cp node_modules/codemirror/mode/javascript/javascript.js build/mode-javascript.js
